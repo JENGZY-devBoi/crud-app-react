8241fb66ad6f739f7fda9cf3ee07ca6e3efdab28
JENGZY-devBoi <teerapong.saisrikaew250819@gmail.com>
2022-04-12 19:28:21 +0700

    made my day

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from './Navbar';
+import User from './User';
+import UserCreate from "./UserCreate";
+import UserUpdate from "./UserUpdate";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/create" element={<UserCreate />} />
+        <Route path="/update/:id" element={<UserUpdate />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
